62a7ea98284839a462227453612659057d490045
huuhuy <huuhuy@3f8a6e4f-5db3-36c9-4280-427330a4dcd6>
2013-11-15 04:40:13 +0000

    git-svn-id: https://codegenerate.googlecode.com/svn/trunk@30 3f8a6e4f-5db3-36c9-4280-427330a4dcd6

--- mygeneration/build.bat
+++ mygeneration/build.bat
@@ -1,4 +1,5 @@
 rem msbuild Zeus.sln /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu"
-rem sbuild MyGeneration\MyGeneration.csproj /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu"
-msbuild MyGeneration.sln  /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu"
+rem msbuild .\MyGeneration\MyGeneration.csproj /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu"
+msbuild MyGeneration.sln  /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu" /target:MyGeneration:Rebuild /p:BuildProjectReferences=false
+rem msbuild MyGeneration.sln  /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu" /target:MyGeneration:Rebuild /p:BuildProjectReferences=false
 rem sbuild MyGeneration.sln  /noconsolelogger /t:Rebuild /p:Configuration=Debug /p:Platform="any cpu"
